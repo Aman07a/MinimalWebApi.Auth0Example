83d44624a10ca47869ab920c8bffba195960e1f0
Aman Ahmed <aman16.aa17@gmail.com>
2023-02-14 11:38:56 +0100

    Securing Minimal API using Auth0 JWT Access Token

--- MinimalWebApi.Auth0Example/Program.cs
+++ MinimalWebApi.Auth0Example/Program.cs
@@ -1,9 +1,59 @@
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
+using Microsoft.OpenApi.Models;
+using System.Text;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddSwaggerGen(options =>
+{
+	options.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+	{
+		In = ParameterLocation.Header,
+		Description = "Authentication Token",
+		Name = "Authorization",
+		Type = SecuritySchemeType.Http,
+		BearerFormat = "JsonWebToken",
+		Scheme = "Bearer"
+	});
+	options.AddSecurityRequirement(new OpenApiSecurityRequirement
+	{
+		{
+			new OpenApiSecurityScheme
+			{
+				Reference = new OpenApiReference
+				{
+					Type = ReferenceType.SecurityScheme,
+					Id = "Bearer"
+				}
+			},
+			new string[]{}
+		}
+	});
+});
+
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+	.AddJwtBearer(JwtBearerDefaults.AuthenticationScheme, c =>
+	{
+		c.Authority = $"https://{builder.Configuration["Auth0:Domain"]}";
+		c.TokenValidationParameters = new TokenValidationParameters
+		{
+			ValidAudience = builder.Configuration["Auth0:Audience"],
+			ValidIssuer = $"{builder.Configuration["Auth0:Domain"]}",
+		};
+	});
+
+builder.Services.AddAuthorization(o =>
+{
+	o.AddPolicy("webpochub:read-write", p => p
+		.RequireAuthenticatedUser()
+		.RequireClaim("scope", "webpochub:read-write"));
+});
 
 var app = builder.Build();
 
